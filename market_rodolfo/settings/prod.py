dff2b49da3cc31ae72ded14f6c8610002e010017
Percy Bellido <pbellido0401@gmail.com>
2026-06-04 21:08:21 -0500

    usando variables  de entorno en prod.py

--- market_rodolfo/settings/prod.py
+++ market_rodolfo/settings/prod.py
@@ -9,11 +9,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mercado_db',
-        'USER': 'rodolfo',
-        'PASSWORD': 'rodolfosuica890',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 
 }
